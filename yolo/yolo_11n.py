7f9140b65becddff306bff895e306c7cf785f728
tomas <tomaber@stud.ntnu.no>
2025-11-11 12:43:53 +0100

    refactor: changed to idun

--- yolo/yolo_11n.py
+++ yolo/yolo_11n.py
@@ -11,9 +11,3 @@ result = model.train(data="/cluster/projects/vc/data/mic/closed/MRI_PVS/roadpole
 
 
 val_results = model.val(data="/cluster/projects/vc/data/mic/closed/MRI_PVS/roadpoles_preprocessed/data.yaml", split="val")
-
-
-print(f"Validation Precision: {val_results.metrics['precision']:.4f}")
-print(f"Validation Recall: {val_results.metrics['recall']:.4f}")
-print(f"Validation mAP@0.5: {val_results.metrics['map50']:.4f}")
-print(f"Validation mAP@0.5:0.95: {val_results.metrics['map50_95']:.4f}")
