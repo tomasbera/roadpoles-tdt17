f97ba046ff838fa2e01143117517d1abb8d45e8e
tomas <tomaber@stud.ntnu.no>
2025-11-11 12:45:23 +0100

    refactor: changed to idun

--- yolo/yolo_11n.py
+++ yolo/yolo_11n.py
@@ -7,7 +7,6 @@ from ultralytics import YOLO
 model_path = "yolo11n.pt"
 
 model = YOLO(model_path)
-result = model.train(data="/cluster/projects/vc/data/mic/closed/MRI_PVS/roadpoles_preprocessed/data.yaml", epochs=2, imgsz=1280)
-
+result = model.train(data="/cluster/projects/vc/data/mic/closed/MRI_PVS/roadpoles_preprocessed/data.yaml", epochs=100, imgsz=1280)
 
 val_results = model.val(data="/cluster/projects/vc/data/mic/closed/MRI_PVS/roadpoles_preprocessed/data.yaml", split="val")
