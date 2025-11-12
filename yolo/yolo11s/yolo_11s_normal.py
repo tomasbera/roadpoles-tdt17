07ac1d3a6daffb23b0bc04a2981430b6c10415b9
tomas <tomaber@stud.ntnu.no>
2025-11-12 09:58:05 +0100

    refactor: new tests

--- yolo/yolo11s/yolo_11s_normal.py
+++ yolo/yolo11s/yolo_11s_normal.py
@@ -8,6 +8,6 @@ from ultralytics import YOLO
 model_path = "../yolo11s.pt"
 
 model = YOLO(model_path)
-result = model.train(data="/cluster/projects/vc/data/mic/closed/MRI_PVS/roadpoles_preprocessed/data.yaml", epochs=200, imgsz=1280)
 
-val_results = model.val(data="/cluster/projects/vc/data/mic/closed/MRI_PVS/roadpoles_preprocessed/data.yaml", split="val")
+result = model.train(data="/cluster/projects/vc/courses/TDT17/ad/Poles2025/roadpoles_v1/data.yaml", epochs=200, imgsz=1280)
+val_results = model.val(data="/cluster/projects/vc/courses/TDT17/ad/Poles2025/roadpoles_v1/data.yaml", split="val")
--- yolo/yolo11s_preprocessed/yolo_11s_preprocessed.py
+++ yolo/yolo11s_preprocessed/yolo_11s_preprocessed.py
@@ -8,6 +8,6 @@ from ultralytics import YOLO
 model_path = "../yolo11s.pt"
 
 model = YOLO(model_path)
-result = model.train(data="../dataset/roadpoles_preprocessed/data.yaml", epochs=200, imgsz=640)
 
-val_results = model.val(data="../dataset/roadpoles_preprocessed/data.yaml", split="val")
+result = model.train(data="/cluster/projects/vc/data/mic/closed/MRI_PVS/roadpoles_preprocessed/data.yaml", epochs=200, imgsz=640)
+val_results = model.val(data="/cluster/projects/vc/data/mic/closed/MRI_PVS/roadpoles_preprocessed/data.yaml", split="val")
